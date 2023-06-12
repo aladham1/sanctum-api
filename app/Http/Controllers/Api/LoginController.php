72e656f09e05c8bb3664965820d1ea044ea9088c
aladham1 <m.aladham@digitalfuture.sa>
2023-06-12 18:10:42 +0300

    first commit

---
+++ app/Http/Controllers/Api/LoginController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class LoginController extends Controller
+{
+    public function login(Request $request)
+    {
+        $user = User::where('email', $request->email)->first();
+
+        if ($user && Hash::check($request->password, $user->password)) {
+            $token = $user->createToken('token-name')->plainTextToken;
+            return response()->json(['token' => $token]);
+        }
+
+        return response()->json(['message' => 'Unauthorized'], 401);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\PostController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+
+Route::middleware('auth:sanctum')->group(function (){
+ Route::get('posts', [PostController::class,'index']);
+});
+Route::post('login', [LoginController::class,'login']);
+
